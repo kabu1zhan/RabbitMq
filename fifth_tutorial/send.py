cf6c2baa5907d45c6e08bb2296246baf4e2fdf30
kabu1zhan <kabiljanz0301@gmail.com>
2021-01-07 18:50:09 +0600

    RabbitMq Tutorial

---
+++ fifth_tutorial/send.py
@@ -0,0 +1,1 @@
+fibonaci_rpc = Fibo
---
+++ second_tutorial/worker.py
@@ -0,0 +1,25 @@
+import time, pika, os
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+print('Wait message')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body.decode())
+    time.sleep(body.count(b'.'))
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
+channel.start_consuming()
+
+if __name__ == '__main__':
+    main()
+    if KeyboardInterrupt:
+        print('Interrupted')
+        quit()
